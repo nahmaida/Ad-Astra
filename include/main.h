54df3b9819c84e88710a0ca64251c45c934313d5
nahmaida <aileeexy@proton.me>
2025-01-19 11:05:28 +0700

    добавил main.h

---
+++ include/main.h
@@ -0,0 +1,45 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "../include/empire.h"
+#include "../include/map.h"
+
+void conquer(System &system, Empire *empire, vector<Empire *> &empires,
+             unordered_map<const Empire *, SDL_Color> empireColors,
+             unordered_map<int, SDL_Color> &systemColors, Galaxy galaxy);
+
+bool isOwned(SDL_Color color);
+
+void updatePower(unordered_map<int, SDL_Color> &systemColors, System &system);
+
+void renderSystemInfo(TTF_Font *font, System *selectedSystem,
+                      SDL_Renderer *renderer, SDL_Rect &infoBox,
+                      const vector<Planet> &planets);
+
+unordered_map<int, SDL_Color> getSystemColors(
+    const vector<System> &systems, vector<Empire *> &empires,
+    unordered_map<const Empire *, SDL_Color> empireColors);
+
+void drawSystem(const System &system, const int CELL_SIZE,
+                System *hoveredSystem, const int HOVER_SIZE,
+                SDL_Renderer *renderer,
+                unordered_map<int, SDL_Color> systemColors, TTF_Font *font);
+
+void drawSystemHover(System *hoveredSystem, int mouseX, int mouseY,
+                     SDL_Renderer *renderer, TTF_Font *font);
+
+void displayGalaxy(const Galaxy &galaxy, vector<Empire *> &empires);
+
+void generateEmpire(vector<Empire *> &empires, Galaxy &galaxy);
+
+#endif  // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
-#include <iostream>
-#include <random>
-#include <string>
-#include <unordered_map>
-#include <vector>
-
-#include "../include/empire.h"
-#include "../include/map.h"
+#include "../include/main.h"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
-const int FONT_SIZE = 24;
+const int FONT_SIZE = 22;
+const int DELAY = 2000;
 
-void conquer(System &system, Empire *empire, std::vector<Empire *> &empires,
-             std::unordered_map<const Empire *, SDL_Color> empireColors,
-             std::unordered_map<int, SDL_Color> &systemColors, Galaxy galaxy) {
+int main(int argc, char *args[]) {
+    loadStarnames("/home/nahmaida/Ad-Astra/res/starnames.txt");
+    Galaxy galaxy(100);
+    galaxy.fill();
+
+    vector<Empire *> empires;
+    while (empires.size() < 9) {
+        generateEmpire(empires, galaxy);
+    }
+
+    displayGalaxy(galaxy, empires);
+
+    for (Empire *empire : empires) {
+        delete empire;
+    }
+
+    return 0;
+}
+
+void conquer(System &system, Empire *empire, vector<Empire *> &empires,
+             unordered_map<const Empire *, SDL_Color> empireColors,
+             unordered_map<int, SDL_Color> &systemColors, Galaxy galaxy) {
     systemColors[system.getId()] = empireColors[empire];
     empire->addSystem(&system);
 }
@@ -32,8 +41,7 @@ bool isOwned(SDL_Color color) {
              color.b == defaultHabitable.b && color.a == defaultHabitable.a);
 }
 
-void updatePower(std::unordered_map<int, SDL_Color> &systemColors,
-                 System &system) {
+void updatePower(unordered_map<int, SDL_Color> &systemColors, System &system) {
     if (isOwned(systemColors[system.getId()])) {
         system.setPower(system.getPower() + 1);
         if (system.hasHabitables()) {
@@ -48,7 +56,7 @@ void updatePower(std::unordered_map<int, SDL_Color> &systemColors,
 
 void renderSystemInfo(TTF_Font *font, System *selectedSystem,
                       SDL_Renderer *renderer, SDL_Rect &infoBox,
-                      const std::vector<Planet> &planets) {
+                      const vector<Planet> &planets) {
     SDL_Color textColor = {205, 214, 244, 255};
     SDL_Color darkTextColor = {30, 30, 46, 255};
     SDL_Color bgGreen = {166, 227, 161, 255};
@@ -90,10 +98,10 @@ void renderSystemInfo(TTF_Font *font, System *selectedSystem,
     }
 }
 
-std::unordered_map<int, SDL_Color> getSystemColors(
-    const std::vector<System> &systems, std::vector<Empire *> &empires,
-    std::unordered_map<const Empire *, SDL_Color> empireColors) {
-    std::unordered_map<int, SDL_Color> systemColors;
+unordered_map<int, SDL_Color> getSystemColors(
+    const vector<System> &systems, vector<Empire *> &empires,
+    unordered_map<const Empire *, SDL_Color> empireColors) {
+    unordered_map<int, SDL_Color> systemColors;
     for (const System &system : systems) {
         SDL_Color systemColor = {249, 226, 175, 255};
         if (system.hasHabitables()) {
@@ -114,8 +122,7 @@ std::unordered_map<int, SDL_Color> getSystemColors(
 void drawSystem(const System &system, const int CELL_SIZE,
                 System *hoveredSystem, const int HOVER_SIZE,
                 SDL_Renderer *renderer,
-                std::unordered_map<int, SDL_Color> systemColors,
-                TTF_Font *font) {
+                unordered_map<int, SDL_Color> systemColors, TTF_Font *font) {
     MapPoint location = system.getLocation();
     int starX = location.x * CELL_SIZE;
     int starY = location.y * CELL_SIZE;
@@ -151,10 +158,35 @@ void drawSystem(const System &system, const int CELL_SIZE,
     SDL_DestroyTexture(texture);
 }
 
-void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
+void drawSystemHover(System *hoveredSystem, int mouseX, int mouseY,
+                     SDL_Renderer *renderer, TTF_Font *font) {
+    const vector<Planet> &planets = hoveredSystem->getPlanets();
+    const int INFO_BOX_WIDTH = 150;
+    const int INFO_BOX_HEIGHT = (planets.size() + 1) * FONT_SIZE;
+    int infoBoxX = mouseX + 10;
+    int infoBoxY = mouseY + 10;
+
+    if (infoBoxX + INFO_BOX_WIDTH > SCREEN_WIDTH) {
+        infoBoxX = mouseX - INFO_BOX_WIDTH - 10;
+    }
+    if (infoBoxY + INFO_BOX_HEIGHT > SCREEN_HEIGHT) {
+        infoBoxY = mouseY - INFO_BOX_HEIGHT - 10;
+    }
+
+    SDL_Rect infoBox = {infoBoxX, infoBoxY, INFO_BOX_WIDTH, INFO_BOX_HEIGHT};
+    SDL_SetRenderDrawColor(renderer, 49, 50, 68, 255);
+    SDL_RenderFillRect(renderer, &infoBox);
+
+    SDL_SetRenderDrawColor(renderer, 205, 214, 244, 255);
+    SDL_RenderDrawRect(renderer, &infoBox);
+
+    renderSystemInfo(font, hoveredSystem, renderer, infoBox, planets);
+}
+
+void displayGalaxy(const Galaxy &galaxy, vector<Empire *> &empires) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
-                  << std::endl;
+        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+             << endl;
         return;
     }
 
@@ -162,15 +194,15 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
                                           SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
                                           SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
-        std::cerr << "Window could not be created! SDL_Error: "
-                  << SDL_GetError() << std::endl;
+        cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
+             << endl;
         SDL_Quit();
         return;
     }
 
     if (TTF_Init() == -1) {
-        std::cerr << "SDL_ttf could not initialize! SDL_ttf Error: "
-                  << TTF_GetError() << std::endl;
+        cerr << "SDL_ttf could not initialize! SDL_ttf Error: "
+             << TTF_GetError() << endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return;
@@ -179,8 +211,8 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
     SDL_Renderer *renderer =
         SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        std::cerr << "Renderer could not be created! SDL_Error: "
-                  << SDL_GetError() << std::endl;
+        cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError()
+             << endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return;
@@ -191,10 +223,10 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
     vector<System> systems = galaxy.getSystems();
     const auto &connections = galaxy.getConnections();
 
-    TTF_Font *font =
-        TTF_OpenFont("/home/nahmaida/Ad-Astra/res/bytebounce_medium.ttf", 22);
+    TTF_Font *font = TTF_OpenFont(
+        "/home/nahmaida/Ad-Astra/res/bytebounce_medium.ttf", FONT_SIZE);
     if (!font) {
-        std::cerr << "Failed to load font: " << TTF_GetError() << std::endl;
+        cerr << "Failed to load font: " << TTF_GetError() << endl;
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         TTF_Quit();
@@ -206,10 +238,10 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
     SDL_Event e;
     System *hoveredSystem = nullptr;
 
-    std::unordered_map<const Empire *, SDL_Color> empireColors;
-    std::random_device rd;
-    std::mt19937 rng(rd());
-    std::uniform_int_distribution<int> dist(50, 255);
+    unordered_map<const Empire *, SDL_Color> empireColors;
+    random_device rd;
+    mt19937 rng(rd());
+    uniform_int_distribution<int> dist(50, 255);
 
     for (const Empire *empire : empires) {
         empireColors[empire] = SDL_Color{static_cast<Uint8>(dist(rng)),
@@ -219,7 +251,7 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
     int i = 0;
 
     while (!quit) {
-        std::unordered_map<int, SDL_Color> systemColors =
+        unordered_map<int, SDL_Color> systemColors =
             getSystemColors(systems, empires, empireColors);
 
         for (Empire *empire : empires) {
@@ -273,32 +305,11 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
         }
 
         if (hoveredSystem) {
-            const std::vector<Planet> &planets = hoveredSystem->getPlanets();
-            const int INFO_BOX_WIDTH = 150;
-            const int INFO_BOX_HEIGHT = (planets.size() + 1) * FONT_SIZE;
-            int infoBoxX = mouseX + 10;
-            int infoBoxY = mouseY + 10;
-
-            if (infoBoxX + INFO_BOX_WIDTH > SCREEN_WIDTH) {
-                infoBoxX = mouseX - INFO_BOX_WIDTH - 10;
-            }
-            if (infoBoxY + INFO_BOX_HEIGHT > SCREEN_HEIGHT) {
-                infoBoxY = mouseY - INFO_BOX_HEIGHT - 10;
-            }
-
-            SDL_Rect infoBox = {infoBoxX, infoBoxY, INFO_BOX_WIDTH,
-                                INFO_BOX_HEIGHT};
-            SDL_SetRenderDrawColor(renderer, 49, 50, 68, 255);
-            SDL_RenderFillRect(renderer, &infoBox);
-
-            SDL_SetRenderDrawColor(renderer, 205, 214, 244, 255);
-            SDL_RenderDrawRect(renderer, &infoBox);
-
-            renderSystemInfo(font, hoveredSystem, renderer, infoBox, planets);
+            drawSystemHover(hoveredSystem, mouseX, mouseY, renderer, font);
         }
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(2000);
+        SDL_Delay(DELAY);
     }
 
     TTF_CloseFont(font);
@@ -308,38 +319,19 @@ void displayGalaxy(const Galaxy &galaxy, std::vector<Empire *> &empires) {
     SDL_Quit();
 }
 
-void generateEmpires(std::vector<Empire *> &empires, Galaxy &galaxy) {
-    while (empires.size() < 9) {
-        int i = empires.size();
-        Empire *empire = new Empire(i + 1);
-        empire->fill(galaxy);
-        for (const Empire *otherEmpire : empires) {
-            if (otherEmpire->getSystems()[0] == empire->getSystems()[0]) {
-                delete empire;
-                empire = nullptr;
-                break;
-            }
-        }
-
-        if (empire) {
-            empires.push_back(empire);
+void generateEmpire(vector<Empire *> &empires, Galaxy &galaxy) {
+    int i = empires.size();
+    Empire *empire = new Empire(i + 1);
+    empire->fill(galaxy);
+    for (const Empire *otherEmpire : empires) {
+        if (otherEmpire->getSystems()[0] == empire->getSystems()[0]) {
+            delete empire;
+            empire = nullptr;
+            break;
         }
     }
-}
 
-int main(int argc, char *args[]) {
-    loadStarnames("/home/nahmaida/Ad-Astra/res/starnames.txt");
-    Galaxy galaxy(100);
-    galaxy.fill();
-
-    std::vector<Empire *> empires;
-    generateEmpires(empires, galaxy);
-
-    displayGalaxy(galaxy, empires);
-
-    for (Empire *empire : empires) {
-        delete empire;
+    if (empire) {
+        empires.push_back(empire);
     }
-
-    return 0;
 }
